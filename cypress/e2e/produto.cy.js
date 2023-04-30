caa50878f5512ad489c3e2cc0a86aa1798ebc05c
fabinhospzn <fabiovideomaker@hotmail.com>
2023-04-30 12:31:54 -0300

    meu primeiro commit

---
+++ cypress/e2e/produto.cy.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress"/>
+var faker = require('faker')
+describe('Funcionalidade Página de Produtos', () => {
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+    it('Deve selecionar produto de uma lista', () => {
+        cy.get('[class="product-block grid"]')
+            //.first()
+            //.last()
+            //.eq(7)
+            .contains('Apollo Running Short')
+            .click()
+    });
+    it('Deve adicionar um produto no carrinho', () => {
+        var quantidade = 3
+        cy.get('[class="product-block grid"]')
+
+        .contains('Abominable Hoodie')
+        .click()
+        cy.get('.button-variable-item-L').click()
+        cy.get('.button-variable-item-Red').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.dropdown-toggle > .mini-cart-items').should ('contain' , quantidade)
+        cy.get('.woocommerce-message').should ('contain' , ' × “Abominable Hoodie” foram adicionados no seu carrinho.')
+    });
+});
